2722b0d2151f0bb26c83e42ee7f9e0308678b4f6
sirine kochbati <sirinesirinekochbati@gmail.com>
2020-07-15 14:17:43 +0200

    third commit

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,3 +1,46 @@
 const express = require('express');
-const router = express.router();
-router.get('/')
+const router = express.Router();
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+
+});
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling POST requests to /products'
+    });
+
+});
+ router.get('/:productID', (req, res, next) => {
+    const id = req.params.productID;
+    if (id == 'special'){
+    res.status(200).json({
+        message: 'you discovered the special id',
+        id :id 
+    });
+}
+    else { 
+        res.status(200).json({
+            message: 'you passed an ID'
+        });
+    }
+});
+router.PATCH('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: 'updateproduct'
+    });
+
+});
+router.DELETE('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: 'deleted product'
+    });
+
+});
+
+   
+
+
+    
+module.exports=router; 
--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,9 +1,5 @@
 const express = require ('express');
 const app = express ();
 const productRoutes = require('./api/routes/products');
-app.use ((req, res, next) =>{
-    res.status(200).json({
-        message: 'IT WORKS!'
-    });
-}) ;
+app.use ('/products', productRoutes);
 module.exports= app ;
