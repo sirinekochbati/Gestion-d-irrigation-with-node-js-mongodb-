09941bc8f5b7edfb1b498a5703e55a9a0da46ff5
sirine kochbati <sirinesirinekochbati@gmail.com>
2020-07-18 18:16:01 +0200

    6th commit

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product');
+const mongoose = require ('mongoose');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,8 +10,22 @@ router.get('/', (req, res, next) => {
 
 });
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling POST requests to /products'
+  
+    const product= new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+
+    });
+    product
+    .save()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
     });
 
 });
